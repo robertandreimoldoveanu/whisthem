536472686dfaa31c0516292a79ccefa1eb198430
Robert Andrei Moldoveanu <robert.moldoveanu97@gmail.com>
2022-01-15 14:32:42 +0200

    feat(api): enable cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,13 +5,13 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.enableCors();
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
